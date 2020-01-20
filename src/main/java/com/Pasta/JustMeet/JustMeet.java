15bde81ac21db07f58baf97c76f7be60e8c906a1
mattia <mattia@localhost>
2020-01-20 12:05:42 +0100

    ciao

--- src/main/java/com/Pasta/JustMeet/JustMeet.java
+++ src/main/java/com/Pasta/JustMeet/JustMeet.java
@@ -20,7 +20,7 @@ public class JustMeet extends SpringBootServletInitializer {
 
 
 
-// TODO : 5.SpringTest (difficult) inter merda
+// TODO : 5.SpringTest (difficult)
 // TODO : 7. factory or builder per la creazione delle notifiche (medium)
 
 
